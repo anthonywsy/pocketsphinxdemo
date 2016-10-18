6edcd5e9757168be6238bf59ca26b9e87077f4a5
anthonywsy <anthony.wsy@gmail.com>
2016-10-18 12:31:40 +0800

    more test at runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -7,4 +7,12 @@ echo $testFile1
 resultFile1="$testFile1.txt"	
 echo $resultFile1
 pocketsphinx_continuous -lm model/en-us/en-us.lm.bin -dict model/en-us/cmudict-en-us.dict -hmm model/en-us/en-us -infile $testFile1 > $resultFile1
-tail $resultFile1
+
+testFile2="test/data/cards/001.wav"
+echo $testFile2
+resultFile2="$testFile2.txt"	
+echo $resultFile2
+pocketsphinx_continuous -lm model/en-us/en-us.lm.bin -dict model/en-us/cmudict-en-us.dict -hmm model/en-us/en-us -infile $testFile2 > $resultFile2
+
+echo "result of test 1: $(tail $resultFile1)"
+echo "result of test 2: $(tail $resultFile2)"
